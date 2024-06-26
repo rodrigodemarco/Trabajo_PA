000e745092f32ef1c65926a7cc71c2fcc2c5e8cc
rodrigodemarco <92384766+rodrigodemarco@users.noreply.github.com>
2024-06-26 20:45:11 -0300

    funciones.py

--- funciones.py
+++ funciones.py
@@ -1,7 +1,9 @@
-
-#Comprueba que el libro no esté alquilado antes de asignarlo a un cliente
-def prestar_libro(cliente, libro):
-    if libro.alquilado_por == None:
-        cliente.libros_alq.append(libro)
+def sumar_copias_disponibles(session, titulo):
+    libros = session.query(Libro).filter(Libro.titulo == titulo).all()
+    if libros:
+        copias_totales = sum(libro.copias_disponibles for libro in libros)
+        print(f"El libro '{titulo}' tiene {copias_totales} copias disponibles.")
+        return copias_totales
     else:
-        print(f"El libro actualmente está alquilado por {libro.alquilado_por}")
+        print(f"No se encontraron libros con el título '{titulo}'.")
+        return 0
