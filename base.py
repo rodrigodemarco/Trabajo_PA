71fb5ffc3fa36e543a034b828f4f0e0d7e96f21a
rodrigodemarco <92384766+rodrigodemarco@users.noreply.github.com>
2024-06-26 20:47:42 -0300

    base.py

--- base.py
+++ base.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import declarative_base, relationship, Mapped, mapped_column
-
+from sqlalchemy.orm import declarative_base, relationship
 
 url_base = "sqlite:///database1.db"
 
@@ -9,26 +8,25 @@ engine = create_engine(url_base)
 Base = declarative_base()
 
 class BaseModel(Base):
-    __abstract__ = True
-    __allow_unmapped__ = True
+    _abstract_ = True
 
     id = Column(Integer, primary_key=True)
 
-
 class Libro(BaseModel):
-    __tablename__ = "libros"
+    _tablename_ = "libros"
 
     titulo = Column(String)
     autor = Column(String)
     resumen = Column(String)
     id_cliente = Column(ForeignKey("clientes.id"))
+    copias_disponibles = Column(Integer, default=1)
     alquilado_por = relationship("Cliente", back_populates="libros_alq")
 
-    def __repr__(self):
+    def _repr_(self):
         return self.titulo
-    
+
 class Cliente(BaseModel):
-    __tablename__ = "clientes"
+    _tablename_ = "clientes"
 
     cuil = Column(Integer, unique=True)
     nombre = Column(String)
@@ -36,8 +34,8 @@ class Cliente(BaseModel):
 
     libros_alq = relationship("Libro", back_populates="alquilado_por")
 
-    def __repr__(self):
+    def _repr_(self):
         return self.nombre
 
-Base.metadata.drop_all(engine) #BORRA TODO EL CONTENIDO
+Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
