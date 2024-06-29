b535a914e63c64bcdd89212a005e9f59951fa154
Agustin-Parisi <143562059+Agustin-Parisi@users.noreply.github.com>
2024-06-29 10:35:09 -0300

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,3 +1,10 @@
+#Comprueba que el libro no esté alquilado antes de asignarlo a un cliente
+def prestar_libro(cliente, libro):
+    if libro.alquilado_por == None:
+        cliente.libros_alq.append(libro)
+    else:
+        print(f"El libro actualmente está alquilado por {libro.alquilado_por}")
+
 def sumar_copias_disponibles(session, titulo):
     libros = session.query(Libro).filter(Libro.titulo == titulo).all()
     if libros:
