a14045ab7db00d220c596ec9f99873e25ac06729
Agustin-Parisi <143562059+Agustin-Parisi@users.noreply.github.com>
2024-06-29 13:16:36 -0300

    Update base.py

--- base.py
+++ base.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import declarative_base, relationship, Mapped
+import datetime as dt
 
 url_base = "sqlite:///database1.db"
 
@@ -8,34 +9,49 @@ engine = create_engine(url_base)
 Base = declarative_base()
 
 class BaseModel(Base):
-    _abstract_ = True
-
+    __abstract__ = True
     id = Column(Integer, primary_key=True)
 
 class Libro(BaseModel):
-    _tablename_ = "libros"
+    __tablename__ = "libros"
 
     titulo = Column(String)
     autor = Column(String)
     resumen = Column(String)
-    id_cliente = Column(ForeignKey("clientes.id"))
     copias_disponibles = Column(Integer, default=1)
-    alquilado_por = relationship("Cliente", back_populates="libros_alq")
 
-    def _repr_(self):
+    def agregar_copia(self):
+        self.copias_disponibles += 1
+    
+    def remover_copia(self):
+        self.copias_disponibles -= 1
+
+    def __repr__(self):
         return self.titulo
 
 class Cliente(BaseModel):
-    _tablename_ = "clientes"
+    __tablename__ = "clientes"
 
     cuil = Column(Integer, unique=True)
     nombre = Column(String)
     edad = Column(Integer)
 
-    libros_alq = relationship("Libro", back_populates="alquilado_por")
-
-    def _repr_(self):
+    def __repr__(self):
         return self.nombre
+
+class Prestacion(BaseModel):
+    __tablename__ = "prestaciones"
+
+    id_cliente = Column(ForeignKey("clientes.id"))
+    id_libro = Column(ForeignKey("libros.id"))
+    fecha_prest = Column(DateTime, default = dt.datetime.combine(dt.date.today(), dt.time.min))
+
+    cliente = relationship("Cliente", backref="prestaciones")
+    libro = relationship("Libro", backref="prestaciones")
+
+    def calcular_dev(self):
+        return self.fecha_prest + dt.timedelta(days=30)
+
 
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
